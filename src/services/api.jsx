52d39a47a04c1e73fd6f78c885ed0f4e1c312812
Amit Kumar <amkr9329@gmail.com>
2025-10-15 12:26:34 +0530

    api change in axios

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://petbackend-guvd.onrender.com",
+  baseURL: "https://petbackend-guvd.onrender.com/api",
   withCredentials: true,
 });
 
